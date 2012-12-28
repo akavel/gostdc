46920e7c2519a490db1edff24d060b33adaee346
Mateusz CzapliĹ„ski <czapkofan@gmail.com>
2012-12-28 20:24:03 +0100

    fix missing 'extern register' declarations by #including <runtime.h>

--- gostdc.h
+++ gostdc.h
@@ -1,85 +1,46 @@
 
-
 /***************************/ 
 /* goctypes.h */ 
 
 #ifndef __goctypes_h
 #define __goctypes_h
 
-// Copyright 2009 The Go Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style
-// license that can be found at the end of this file.
+// We don't need to provide path to runtime.h, as it is distributed
+// in $GOROOT/pkg/$GOOS_$GOARCH, and the 'go' tool passes this path
+// to the internal C compiler via -I flag. Who would expect.
+//
+// From runtime.h, we currently need a few VERY IMPORTANT items:
+// - extern register M *m; extern register G *g;
+//   See:
+//    https://code.google.com/p/go/source/diff?spec=svn9ef1fd2b7e476df9c04fec3c9833fe473cbacc42&name=9ef1fd2b7e47&r=9ef1fd2b7e476df9c04fec3c9833fe473cbacc42&format=side&path=/src/pkg/runtime/runtime.h#sc_svn9ef1fd2b7e476df9c04fec3c9833fe473cbacc42_94
+//    http://doc.cat-v.org/plan_9/4th_edition/papers/compiler
+//   In short words, *every* C file linked to Go *must* have identical
+//   "extern register" declarations, so compiler knows to reserve that
+//   many registers for those special variables.
+// - uintptr, intptr (depends on $GOARCH)
+// - ROUND() macro (varargs memory layout)
+//
+// Unfortunately, this will also pollute our global namespace with a lot
+// of other symbols. If that becomes a problem, we might need to
+// drop this #include, and import required items by hand, but until
+// we have to, it's easier, safer and more portable to #include.
+#include <runtime.h>
 
-/*
- * basic types
- */
-typedef	signed char		int8;
-typedef	unsigned char		uint8;
-typedef	signed short		int16;
-typedef	unsigned short		uint16;
-typedef	signed int		int32;
-typedef	unsigned int		uint32;
-typedef	signed long long int	int64;
-typedef	unsigned long long int	uint64;
-typedef	float			float32;
-typedef	double			float64;
+#undef unsigned
+#undef signed
+#undef char
+#undef short
+#undef int
+#undef long
+#undef float
+#undef double
 
-#ifdef _64BIT
-typedef	uint64		uintptr;
-typedef	int64		intptr;
-#else
-typedef	uint32		uintptr;
-typedef int32		intptr;
+// See:
+// https://codereview.appspot.com/6260051
+// http://code.google.com/p/go/source/detail?r=722bb90ae3ee
+#ifndef ROUND
+#define ROUND runtime·rnd
 #endif
-
-#define	USED(x)	if(x){}else{}
-#define	FLUSH(x)	USED(x)
-
-#define	ROUND(x, n)	(((x)+(n)-1)&~((n)-1)) /* all-caps to mark as macro: it evaluates n twice */
-
-/*
- * get rid of C types
- * the / / / forces a syntax error immediately,
- * which will show "last name: XXunsigned".
- */
-/*
-#define	unsigned		XXunsigned / / /
-#define	signed			XXsigned / / /
-#define	char			XXchar / / /
-#define	short			XXshort / / /
-#define	int			XXint / / /
-#define	long			XXlong / / /
-#define	float			XXfloat / / /
-#define	double			XXdouble / / /
-*/
-
-// Copyright (c) 2009 The Go Authors. All rights reserved.
-// 
-// Redistribution and use in source and binary forms, with or without
-// modification, are permitted provided that the following conditions are
-// met:
-// 
-//    * Redistributions of source code must retain the above copyright
-// notice, this list of conditions and the following disclaimer.
-//    * Redistributions in binary form must reproduce the above
-// copyright notice, this list of conditions and the following disclaimer
-// in the documentation and/or other materials provided with the
-// distribution.
-//    * Neither the name of Google Inc. nor the names of its
-// contributors may be used to endorse or promote products derived from
-// this software without specific prior written permission.
-// 
-// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
-// A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
-// OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
-// SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
-// LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
-// DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
-// THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-// OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 #endif
 
